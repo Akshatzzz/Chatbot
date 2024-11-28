bdae0170ea0954c465c92deada25b463ac2ee5f5
akshat <akshat.chaturvedi@bobble.ai>
2024-11-29 00:12:47 +0530

    implemented di and mvvm architecture

--- app/src/main/java/com/example/geminichatbot/presentation/ChatEditText.kt
+++ app/src/main/java/com/example/geminichatbot/presentation/ChatEditText.kt
@@ -17,10 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,14 +25,12 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.geminichatbot.R
 import com.example.geminichatbot.data.TextualModelInputImpl
 import com.example.geminichatbot.domain.ConversationEvent
 import com.example.geminichatbot.domain.ConversationViewModel
-import com.example.geminichatbot.ui.theme.PurpleChat
 
 
 @Composable
